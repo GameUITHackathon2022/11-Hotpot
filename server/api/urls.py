12c28c7fdae4548d6ee28e665656fc42bbff2164
winprn <minhladz001@gmail.com>
2022-11-27 07:51:51 +0700

    add server

---
+++ server/api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('login', views.login),
+    path('register', views.register),
+    path('vendors', views.get_all_vendors),
+    path('vendor/<int:id>', views.get_single_vendor),
+    path('trees', views.get_all_basetrees),
+    path('trees/<str:space>', views.get_basetrees_with_space),
+    path('tree/<int:id>', views.get_single_basetree),
+    path('add_tree', views.add_tree),
+    path('user/<int:user_id>/trees', views.get_trees_of_user),
+    path('user/<int:user_id>/checklist', views.check_checklist),
+    path('task/<str:task_name>/<int:tree_id>',
+         views.tick_checklist),
+    path('user/<int:user_id>/check-streak', views.check_streak),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
